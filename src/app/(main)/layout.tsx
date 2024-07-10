e8de6e7bbba9b59d086a360c0c55e42ccfefe6bd
uni51 <unieye51@gmail.com>
2024-07-10 13:56:41 +0900

    25. メインページの実装

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ const MainLayout = ({
   return (
     <div className="flex h-screen">
       <SideMenu />
-      <main className="bg-red-300 flex-1 overflow-auto">{children}</main>
+      <main className="bg-slate-50 flex-1 overflow-auto">{children}</main>
     </div>
   );
 };
