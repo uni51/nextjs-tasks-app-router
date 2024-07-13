e3df34fcd0e784f5a96f8d026ec90f099d6b4ac8
uni51 <unieye51@gmail.com>
2024-07-13 10:01:00 +0900

    26. タスクカードの実装 Part1

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import TaskCard from "@/components/SideMenu/TaskCard/TaskCard";
 import Link from "next/link";
 import { MdAddTask } from "react-icons/md";
 
@@ -14,7 +15,9 @@ export default function MainPage() {
           <div>Add Task</div>
         </Link>
       </header>
-      <div className="mt-8 flex flex-wrap gap-4">Task Card</div>
+      <div className="mt-8 flex flex-wrap gap-4">
+        <TaskCard />
+      </div>
     </div>
   );
 }
