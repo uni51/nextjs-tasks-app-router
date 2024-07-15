ac6da9b5d7f33e662c3604f79d7cd507a2c15ba3
uni51 <unieye51@gmail.com>
2024-07-15 16:01:56 +0900

    42. IDによるタスク取得APIの実装

--- src/app/(main)/edit/[id]/page.tsx
+++ src/app/(main)/edit/[id]/page.tsx
@@ -1,11 +1,23 @@
 import EditTaskForm from "@/components/EditTaskForm/EditTaskForm";
+import { TaskDocument } from "@/models/task";
+import { cache } from "react";
 
 interface Params {
   params: { id: string };
 }
 
-const EditTaskPage = ({ params }: Params) => {
-  // const id = params.id;
+const getTask = async (id: string): Promise<TaskDocument> => {
+  const response = await fetch(`${process.env.API_URL}/tasks/${id}`, {
+    cache: "no-store",
+  });
+
+  const data = await response.json();
+  return data.task as TaskDocument;
+};
+
+const EditTaskPage = async ({ params }: Params) => {
+  const id = params.id;
+  const task = await getTask(id);
 
   return (
     <div className="flex flex-col justify-center py-20">
