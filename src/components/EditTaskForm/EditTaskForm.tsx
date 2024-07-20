c4e9a134b6e941a8e411319b76b9e20b3b9cd08e
uni51 <unieye51@gmail.com>
2024-07-20 11:41:19 +0900

    43. タスク編集機能の実装 Part1

--- src/components/EditTaskForm/EditTaskForm.tsx
+++ src/components/EditTaskForm/EditTaskForm.tsx
@@ -1,6 +1,20 @@
-const EditTaskForm = () => {
+"use client";
+
+import { TaskDocument } from "@/models/task";
+import { useState } from "react";
+
+interface EditTaskFormProps {
+  task: TaskDocument;
+}
+
+const EditTaskForm: React.FC<EditTaskFormProps> = ({ task }) => {
+  const [title, setTitle] = useState(task.title);
+  const [description, setDescription] = useState(task.description);
+  const [dueDate, setDueDate] = useState(task.dueDate);
+  const [isCompleted, setIsCompleted] = useState(task.isCompleted);
+
   return (
-    <div className="mt-10 mx-auto w-fill max-w-sm">
+    <div className="mt-10 mx-auto w-full max-w-md">
       <form action="">
         <div>
           <label htmlFor="title" className="block text-sm font-medium">
@@ -10,6 +24,8 @@ const EditTaskForm = () => {
             type="text"
             id="title"
             name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             required
             className="block mt-2 py-1.5 px-2 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300"
           />
@@ -22,6 +38,8 @@ const EditTaskForm = () => {
             type="text"
             id="description"
             name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             required
             className="block mt-2 py-1.5 px-2 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300"
           />
@@ -34,6 +52,8 @@ const EditTaskForm = () => {
             type="date"
             id="dueDate"
             name="dueDate"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
             min="2020-01-01"
             max="2999-12-31"
             required
@@ -45,6 +65,8 @@ const EditTaskForm = () => {
             type="checkbox"
             id="isCompleted"
             name="isCompleted"
+            checked={isCompleted}
+            onChange={(e) => setIsCompleted(e.target.checked)}
             className="mr-2 w-4 h-4"
           />
           <label htmlFor="isCompleted" className="text-sm font-medium">
