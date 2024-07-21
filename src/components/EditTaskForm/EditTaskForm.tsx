94e16a575d546bbc6851f4850d34946fb76c2ba1
uni51 <unieye51@gmail.com>
2024-07-21 14:14:34 +0900

    44. タスク編集機能の実装 Part2

--- src/components/EditTaskForm/EditTaskForm.tsx
+++ src/components/EditTaskForm/EditTaskForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { FormState, updateTask } from "@/actions/task";
 import { TaskDocument } from "@/models/task";
 import { useState } from "react";
+import { useFormState, useFormStatus } from "react-dom";
 
 interface EditTaskFormProps {
   task: TaskDocument;
@@ -13,9 +15,27 @@ const EditTaskForm: React.FC<EditTaskFormProps> = ({ task }) => {
   const [dueDate, setDueDate] = useState(task.dueDate);
   const [isCompleted, setIsCompleted] = useState(task.isCompleted);
 
+  const updateTaskWithId = updateTask.bind(null, task._id);
+  const initialState: FormState = { error: "" };
+  const [state, formAction] = useFormState(updateTaskWithId, initialState);
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <button
+        type="submit"
+        disabled={pending}
+        className="mt-8 py-2 w-full rounded-md text-white bg-gray-800 hover:bg-gray-700 text-sm font-semibold shadow-sm disabled:bg-gray-400"
+      >
+        Edit
+      </button>
+    );
+  };
+
   return (
     <div className="mt-10 mx-auto w-full max-w-md">
-      <form action="">
+      <form action={formAction}>
         <div>
           <label htmlFor="title" className="block text-sm font-medium">
             タイトル
@@ -73,12 +93,10 @@ const EditTaskForm: React.FC<EditTaskFormProps> = ({ task }) => {
             タスクを完了にする
           </label>
         </div>
-        <button
-          type="submit"
-          className="mt-8 py-2 w-full rounded-md text-white bg-gray-800 hover:bg-gray-700 text-sm font-semibold shadow-sm"
-        >
-          Edit
-        </button>
+        <SubmitButton />
+        {state.error !== "" && (
+          <p className="mt-2 text-red-500 text-sm">{state.error}</p>
+        )}
       </form>
     </div>
   );
